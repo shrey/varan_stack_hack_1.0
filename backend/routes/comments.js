227b4973df4753b8d516cc953cfc5ffcebf8218b
Riyan Dhiman <Riyandhiman14@gmail.com>
2020-05-20 20:56:15 +0530

    API for comments

---
+++ backend/routes/comments.js
@@ -0,0 +1,39 @@
+var router = require('express').Router()
+var mongodb = require('mongodb')
+
+const MONGO_URI = 'mongodb://localhost'
+
+router.put('/', (req, res, next)=>{
+
+	mongo.MongoClient.connect(MONGO_URI, (error, client)=>{
+
+		var event_db = client.db('varan').collection('event')
+
+		event_db.findOne({ _id : new mongo.ObjectId(req.body._id)}, (err, event)=>{
+			var comment = event.comment;
+			var data = {
+				user : req.body.name,
+				comment : req.body.comment
+			};
+
+			if(comment == undefined)
+				comment = [data];
+			else
+				comment.push(data);
+
+			event_db.updateOne({ _id : new mongo.ObjectId(req.body._id)}, { $set : { comment : comment } }, (error_update, update)=>{
+
+				if(error_update)
+					res.status(400).json({"msg" : "Cannot make Comment"});
+			});
+
+			event_db.findOne({ _id : new mongo.ObjectId(req.body._id)}, (error_event, updated_event)=>{
+
+				res.status(200).json(updated_event);
+			});
+
+		});
+	});
+});
+
+module.exports = router;
